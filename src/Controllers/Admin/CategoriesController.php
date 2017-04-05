9544332f8b2e7f29dbdf5144fcf0b9ef260cf9d6
Craig Liddle <craigliddle2@gmail.com>
2017-04-05 22:09:35 +0100

    remove unused code

--- src/Controllers/Admin/CategoriesController.php
+++ src/Controllers/Admin/CategoriesController.php
@@ -48,7 +48,7 @@ class CategoriesController
 
     public function deleteCategory()
     {
-        
+
     }
 
 }
--- src/Controllers/Admin/HomeController.php
+++ src/Controllers/Admin/HomeController.php
@@ -3,79 +3,12 @@
 namespace LiddleDev\LiddleForum\Controllers\Admin;
 
 use Illuminate\Http\Request;
-use LiddleDev\LiddleForum\Models\Admin;
-use LiddleDev\LiddleForum\Models\Category;
-use LiddleDev\LiddleForum\Models\Moderator;
 
 class HomeController
 {
     public function getIndex()
     {
-        $admins = Admin::with('user')->get();
-        $moderators = Moderator::with('user')->with('category')->get();
-
-        $categories = Category::orderBy('parent_id')->get();
-
-        $baseCategories = Category::whereNull('parent_id')->get();
-
-        return view('liddleforum::admin.index', [
-            'admins' => $admins,
-            'moderators' => $moderators,
-            'categories' => $categories,
-            'baseCategories' => $baseCategories,
-        ]);
-    }
-
-    public function deleteAdmin(Request $request, $admin_id)
-    {
-        if ( ! $admin = $this->fetchAdmin($admin_id)) {
-            abort(404);
-        }
-
-        $admin->delete();
-
-        $request->session()->flash('liddleforum_success', 'Admin privileges have been revoked');
-        return redirect()->route('liddleforum.admin.index');
-    }
-
-    public function postCreateAdmin(Request $request)
-    {
-
-    }
-
-    public function deleteModerator(Request $request, $moderator_id)
-    {
-        if ( ! $moderator = $this->fetchModerator($moderator_id)) {
-            abort(404);
-        }
-
-        $moderator->delete();
-
-        $request->session()->flash('liddleforum_success', 'Moderator privileges have been revoked');
-        return redirect()->route('liddleforum.admin.index');
-    }
-
-    public function postCreateModerator(Request $request)
-    {
-
-    }
-
-    /**
-     * @param $moderator_id
-     * @return Moderator|null
-     */
-    protected function fetchModerator($moderator_id)
-    {
-        return Moderator::where('id', '=', $moderator_id)->first();
-    }
-
-    /**
-     * @param $admin_id
-     * @return Admin|null
-     */
-    protected function fetchAdmin($admin_id)
-    {
-        return Admin::where('id', '=', $admin_id)->first();
+        return view('liddleforum::admin.index', []);
     }
 
 }
